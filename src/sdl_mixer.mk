2b6b881684c75c6804db071d9199de4134b28a11
Yankes <Yankes@users.noreply.github.com>
2015-06-24 19:32:14 +0200

    Add flac to sdl_mixer

--- src/sdl_mixer.mk
+++ src/sdl_mixer.mk
@@ -8,7 +8,7 @@ $(PKG)_CHECKSUM := a20fa96470ad9e1052f1957b77ffa68fb090b384
 $(PKG)_SUBDIR   := SDL_mixer-$($(PKG)_VERSION)
 $(PKG)_FILE     := SDL_mixer-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.libsdl.org/projects/SDL_mixer/release/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc sdl libmodplug ogg vorbis smpeg
+$(PKG)_DEPS     := gcc sdl libmodplug ogg vorbis smpeg flac
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://hg.libsdl.org/SDL_mixer/tags' | \
@@ -19,7 +19,7 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    $(SED) -i 's,^\(Requires:.*\),\1 vorbisfile,' '$(1)/SDL_mixer.pc.in'
+    $(SED) -i 's,^\(Requires:.*\),\1 vorbisfile flac,' '$(1)/SDL_mixer.pc.in'
     echo \
         'Libs.private:' \
         "`$(TARGET)-pkg-config libmodplug --libs`" \
@@ -35,7 +35,7 @@ define $(PKG)_BUILD
         --disable-music-mod \
         --enable-music-mod-modplug \
         --enable-music-ogg \
-        --disable-music-flac \
+        --enable-music-flac \
         --enable-music-mp3 \
         --disable-music-mod-shared \
         --disable-music-ogg-shared \
@@ -44,7 +44,7 @@ define $(PKG)_BUILD
         --disable-smpegtest \
         --with-smpeg-prefix='$(PREFIX)/$(TARGET)' \
         WINDRES='$(TARGET)-windres' \
-        LIBS='-lvorbis -logg'
+        LIBS='-lvorbis -logg -lFLAC'
     $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 
     '$(TARGET)-gcc' \
